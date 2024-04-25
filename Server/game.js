60f419fbb7de8df4ae540fd2688b619637905fa1
Pieter-Jan-DAE <pieter-jan.vandenberghe@howest.be>
2024-04-25 19:03:41 +0200

    Add server code

---
+++ Server/game.js
@@ -0,0 +1,149 @@
+import { v4 as uuid } from 'uuid';
+
+export class Game {
+    constructor(boundaries) {
+        this.boundaries = boundaries != undefined ? boundaries : { top: 1000, left: 0, right: 1000, bottom: 0 };
+        this.players = {};
+        const victim = new Player({ x: 0, y: 0 });
+        this.players[victim.id] = victim;
+        this.attire = {};
+        const a = new Attire({ x: 100, y: 100 }, Attire.TYPES.HAT);
+        this.attire[a.id] = a;
+        this.stateChanged = false;
+    }
+
+    addPlayer() {
+        const p = new Player({ x: 300, y: 300 });
+        this.players[p.id] = p;
+        this.stateChanged = true;
+        return p.id;
+    }
+
+    move(playerId, vector) {
+        if (!(playerId in this.players)) {
+            console.log(`Attempt to move with unknown playerId: ${playerId}`);
+            return;
+        }
+        if (!vector || !('x' in vector) || !('y' in vector) || typeof vector.x !== 'number' || typeof vector.y !== 'number')
+        {
+            console.log(`Attempt to move with malformed vector: ${vector}`);
+            return;
+        }
+        if (playerId in this.players) {
+            const p = this.players[playerId];
+            p.pos.x += vector.x;
+            p.pos.y += vector.y;
+            const b = this.boundaries;
+            const epsilon = 0.001;
+            if (p.pos.x < b.left) p.pos.x = b.left + epsilon;
+            if (p.pos.x > b.right) p.pos.x = b.right - epsilon;
+            if (p.pos.y < b.bottom) p.pos.y = b.bottom + epsilon;
+            if (p.pos.y > b.top) p.pos.y = b.top - epsilon;
+            this.stateChanged = true;
+        }
+    }
+
+    take(playerId, attireId) {
+        if (!(playerId in this.players)) {
+            console.log(`Attempt to take with unknown playerId: ${playerId}`);
+            return;
+        }
+        if (!(attireId in this.attire)) {
+            console.log(`Attempt to take with unknown attireId: ${attireId}`);
+            return;
+        }
+        const player = this.players[playerId];
+        const attire = this.attire[attireId];
+        if (player.add(attire.type)) {
+            delete this.attire[attireId];
+            this.stateChanged = true;
+        }
+    }
+
+    drop(playerId, attireType) {
+        if (!(playerId in this.players)) {
+            console.log(`Attempt to drop with unknown playerId: ${playerId}`);
+            return;
+        }
+        if (this.players[playerId].attire[attireType]) {
+            const player = this.players[playerId];
+            const pos = { x: player.pos.x, y: player.pos.y };
+            const attire = new Attire(pos, attireType);
+            this.attire[attire.id] = attire;
+            player.remove(attireType);
+            this.stateChanged = true;
+        }
+    }
+
+    attack(aggressorId, victimId) {
+        if (!(aggressorId in this.players)) {
+            console.log(`Attempt to attack failed, unknown aggressorId: ${aggressorId}`);
+            return;
+        }
+        if (!(victimId in this.players)) {
+            console.log(`Attempt to attack failed, unknown victimId: ${victimId}`);
+            return;
+        }
+        if (!this.players[aggressorId].isInSameCult(this.players[victimId])) {
+            this.players[victimId].health -= 20;
+            this.stateChanged = true;
+        }
+    }
+
+    getState() {
+        return { players: this.players, attire: this.attire };
+    }
+
+    hasStateChanged() {
+        return this.stateChanged;
+    }
+}
+
+export class GameObject {
+    constructor(pos) {
+        this.id = uuid();
+        this.pos = pos;
+    }
+}
+
+export class Player extends GameObject {
+    constructor(pos) {
+        super(pos);
+        this.health = 100;
+        this.maxHealth = 100;
+        this.attire = {};
+        for (let type of Object.values(Attire.TYPES)) {
+            this.attire[type] = false;
+        }
+    }
+
+    add(attireType) {
+        if (this.attire[attireType]) return false;
+        this.attire[attireType] = true;
+        return true;
+    }
+
+    remove(attireType) {
+        this.attire[attireType] = false;
+    }
+
+    isInSameCult(player) {
+        for (let type of Object.values(Attire.TYPES)) {
+            if (player.attire[type] !== this.attire[type]) return false;
+        }
+        return true;
+    }
+}
+
+export class Attire extends GameObject {
+    constructor(pos, type) {
+        super(pos);
+        this.type = type;
+    }
+
+    static TYPES = {
+        HAT: 0,
+        CANE: 1,
+        OTHER: 2,
+    }
+}
---
+++ Server/server.js
@@ -0,0 +1,74 @@
+import Koa from 'koa';
+import Router from 'koa-router';
+import bodyParser from '@koa/bodyparser';
+import { Game } from './game.js';
+
+const app = new Koa();
+
+// logger
+app.use(async (ctx, next) => {
+    await next();
+    const rt = ctx.response.get('X-Response-Time');
+    console.log(`${ctx.method} ${ctx.url} - ${rt}`);
+});
+
+// x-response-time
+app.use(async (ctx, next) => {
+    const start = Date.now();
+    await next();
+    const ms = Date.now() - start;
+    ctx.set('X-Response-Time', `${ms}ms`);
+});
+
+// body parsing
+app.use(bodyParser());
+
+// game state
+const game = new Game();
+let stateStringified = JSON.stringify(game.getState());
+
+const router = new Router();
+router.get('/state', (ctx, next) => {
+    if (game.hasStateChanged()) stateStringified = JSON.stringify(game.getState());
+    ctx.body = stateStringified;
+    ctx.set('Content-Type', 'application/json');
+});
+
+router.post('/player', (ctx, next) => {
+    const id = game.addPlayer()
+    ctx.body = { id };
+});
+
+router.put('/player/:id/move', (ctx, next) => {
+    const playerId = ctx.params.id;
+    const body = ctx.request.body;
+    game.move(playerId, body.vector);
+    ctx.body = game.getState().players[playerId];
+});
+
+router.put('/player/:id/take', (ctx, next) => {
+    const playerId = ctx.params.id;
+    const body = ctx.request.body;
+    game.take(playerId, body.id);
+    ctx.body = game.getState().players[playerId];
+});
+
+router.put('/player/:id/drop', (ctx, next) => {
+    const playerId = ctx.params.id;
+    const body = ctx.request.body;
+    game.drop(playerId, body.type);
+    ctx.body = game.getState().players[playerId];
+});
+
+router.put('/player/:id/attack', (ctx, next) => {
+    const playerId = ctx.params.id;
+    const body = ctx.request.body;
+    game.attack(playerId, body.id);
+    ctx.body = game.getState().players[body.id];
+});
+
+app.use(router.routes())
+
+app.listen(3000, () => {
+    console.log('Server running on https://localhost:3000')
+});
